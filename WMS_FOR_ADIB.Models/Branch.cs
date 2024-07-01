74cf840029421ea38fbc21ebec92298ae6d8641d
Ashenafi-Bir <Ashenafibirhanu33@gmail.com>
2024-07-01 13:59:50 +0300

    Add new model

---
+++ WMS_FOR_ADIB.Models/Branch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMS_FOR_ADIB.Models
+{
+    public class Branch
+    {
+
+        [Key]
+        public int BranchID { get; set; }
+
+        [Required]
+        [DisplayName("Branch Name")]
+        public string? BranchName { get; set; }
+
+        [Required]
+        [DisplayName("Branch Code")]
+        public string? BranchCode { get; set; }
+
+        // Navigation property
+        public ICollection<ApplicationUser>? ApplicationUsers { get; set; }
+    }
+}
